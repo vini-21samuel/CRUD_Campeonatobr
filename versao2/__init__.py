fa2d8f16ddf1620cf5fce1f67dbb56ee33875669
Vinicius Samuel <130195075+vini-21samuel@users.noreply.github.com>
2024-12-12 22:36:09 -0300

    Delete versao2 directory

--- versao2/__init__.py
+++
@@ -1,5 +0,0 @@
-# Exemplo de __init__.py com funções ou variáveis
-from .database import SessionLocal, engine
-from .models import Time, Jogador
-
-__all__ = ['SessionLocal', 'engine', 'Time', 'Jogador']
