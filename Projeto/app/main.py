bc56ef7059537e870e27d4fdbe550b169f19bcb8
vini-21samuel <viniciussamuel1277@gmail.com>
2025-03-30 13:47:10 -0300

    corrigindo rota raiz

--- Projeto/app/main.py
+++ Projeto/app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 from database.database import get_db
-from app.models.models import *  # Importando os modelos corretamente
-
+from app.utils.auth import decode_token  # Importando a função correta
 
 # Instanciando o FastAPI
 app = FastAPI()
@@ -13,12 +12,17 @@ app = FastAPI()
 # Configuração de templates Jinja2
 templates = Jinja2Templates(directory="app/templates")
 
-# Serve arquivos estáticos (como CSS, imagens, etc.)
+# Serve arquivos estáticos (CSS, imagens, etc.)
 app.mount("/static", StaticFiles(directory="app/templates/static/"), name="static")
 
 # Rota para renderizar a página inicial
 @app.get("/", response_class=HTMLResponse)
-async def listar_partidas_html(request: Request, db: Session = Depends(get_db)):
+async def home(request: Request, db: Session = Depends(get_db)):
+    token = request.cookies.get("access_token")
+
+    if not token or not decode_token(token):  # Usa a função para validar o token
+        return RedirectResponse(url="/usuarios/login", status_code=303)
+
     return templates.TemplateResponse("base.html", {"request": request})
 
 # Importação das rotas após a criação do `app`
@@ -28,11 +32,9 @@ from app.routes.partidas import router as partidas_router
 from app.routes.usuarios import router as usuarios_router
 from app.routes.torneios import router as torneios_router
 
-
 # Incluindo as rotas no FastAPI
 app.include_router(usuarios_router, prefix="/usuarios", tags=["Usuarios"])
 app.include_router(times_router, prefix="/times", tags=["Times"])
 app.include_router(jogadores_router, prefix="/jogadores", tags=["Jogadores"])
 app.include_router(partidas_router, prefix="/partidas", tags=["Partidas"])
 app.include_router(torneios_router, prefix="/torneios", tags=["Torneios"])
-
