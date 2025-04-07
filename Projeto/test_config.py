3cd5791e02c670256a91895c19d4a66b92070461
vini-21samuel <viniciussamuel1277@gmail.com>
2025-04-06 21:47:45 -0300

    consertando um bug do banco

---
+++ Projeto/test_config.py
@@ -0,0 +1,7 @@
+from app.config import settings
+
+print(f"DATABASE_PORT: {settings.DATABASE_PORT}")
+print(f"POSTGRES_PASSWORD: {settings.POSTGRES_PASSWORD}")
+print(f"POSTGRES_USER: {settings.POSTGRES_USER}")
+print(f"POSTGRES_DB: {settings.POSTGRES_DB}")
+print(f"POSTGRES_HOST: {settings.POSTGRES_HOST}")
